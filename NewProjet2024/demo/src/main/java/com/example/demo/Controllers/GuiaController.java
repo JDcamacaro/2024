80f7f9b68b93dd51a9433513429ec0bd65fefb12
Jesus Camacaro <davidjcamacaro@gmail.com>
2024-10-02 22:53:38 -0400

    2/10/24

--- NewProjet2024/demo/src/main/java/com/example/demo/Controllers/GuiaController.java
+++ NewProjet2024/demo/src/main/java/com/example/demo/Controllers/GuiaController.java
@@ -7,10 +7,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 
-@Restcontroller
+@RestController
 @RequestMapping(path = "/guia")
 public class GuiaController {
     @Autowired
